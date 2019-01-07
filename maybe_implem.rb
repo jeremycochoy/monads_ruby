d55caa412abf1d17f1f088500e37709b48fdc4a4
Jeremy Cochoy <jeremy.cochoy@gmail.com>
2019-01-07 09:19:56 +0100

    First part on monads ok

--- maybe_implem.rb
+++ maybe_implem.rb
@@ -32,3 +32,17 @@ end
 
 what_is(Just.(3))
 what_is(Nothing.())
+
+
+def fmap(f)
+  ->(box) do
+    case box
+    when Nothing
+      return Nothing.()
+    when Just
+      r = f.(box.from_maybe)
+      Just.(r)
+    end
+  end
+end
+
